70d89e06ae6d959d5b270c0caef769d60e165483
Simeon-Paris <alupar9905@ieselcaminas.org>
2023-03-02 17:44:51 +0100

    vamos a hacer un Merge

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Primera refactorizacion");
         System.out.println("CAMBIO HECHO PARA HACER UN PULL (SOLICITUD DE EXTRACCIÓN)");
+        System.out.println("Vamos a hacer un Merge");
     }
 }
